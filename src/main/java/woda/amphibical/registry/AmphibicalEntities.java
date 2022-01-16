0c2b16c6ebfeec9154e47eaab350bd42a4bdfe63
Coda <cody.koelliker@outlook.com>
2022-01-15 21:18:05 -0700

    im off my gloopty!

--- src/main/java/woda/amphibical/registry/AmphibicalEntities.java
+++ src/main/java/woda/amphibical/registry/AmphibicalEntities.java
@@ -12,7 +12,7 @@ import woda.amphibical.common.entity.PinocchioSwordmasterEntity;
 public class AmphibicalEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, Amphibical.MOD_ID);
 
-    public static final RegistryObject<EntityType<PinocchioSwordmasterEntity>> PINOCCHIO_SWORDMASTER = create("pinocchio_swordmaster", EntityType.Builder.of(PinocchioSwordmasterEntity::new, MobCategory.MONSTER).sized(0.6f, 185f));
+    public static final RegistryObject<EntityType<PinocchioSwordmasterEntity>> PINOCCHIO_SWORDMASTER = create("pinocchio_swordmaster", EntityType.Builder.of(PinocchioSwordmasterEntity::new, MobCategory.MONSTER).sized(0.6f, 1.85f));
 
     private static <T extends Entity> RegistryObject<EntityType<T>> create(String name, EntityType.Builder<T> builder) {
         return ENTITIES.register(name, () -> builder.build(Amphibical.MOD_ID + "." + name));
